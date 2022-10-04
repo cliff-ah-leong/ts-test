298d7cdb5abd854c82e86d749b27282fafaeb270
Cliff Leong <cliff1228leong@gmail.com>
2022-10-04 14:26:18 -0400

    Cliff's result

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,19 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
 // TODO define this type properly
-type TResult = any;
+type TResult = Partial<{
+  "Arnie Quote": string;
+  FAILURE: string;
+}>;
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
   // TODO: Implement this function.
-  
-  return [];
+  const promises = urls.map((url) => httpGet(url));
+  return Promise.all(promises).then((results) =>
+    results.map((result) =>
+      result.status === 200
+        ? { "Arnie Quote": JSON.parse(result.body).message }
+        : { FAILURE: JSON.parse(result.body).message }
+    )
+  );
 };
